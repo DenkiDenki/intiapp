04014612e0bd720a5b40ac81d0fcc123f618fb8d
Denisa <denisa.gerez@gmail.com>
2022-08-26 23:09:31 -0300

    create the list of basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorkExperienceComponent } from './components/work-experience/work-experience.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CareerObjectiveComponent } from './components/career-objective/career-objective.component';
+import { ProfileTitleComponent } from './components/profile-title/profile-title.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SlideComponent } from './components/slide/slide.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    EducationComponent,
+    SkillsComponent,
+    WorkExperienceComponent,
+    ContactComponent,
+    CareerObjectiveComponent,
+    ProfileTitleComponent,
+    ProjectsComponent,
+    FooterComponent,
+    SlideComponent,
+    ContactFormComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule
